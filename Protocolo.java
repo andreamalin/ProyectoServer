d84106d71a242cb5b8c121e081d0023723fbf839
andreamalin <andreaa.malin@gmail.com>
2020-11-05 20:59:30 -0600

    data base simulado

--- Protocolo.java
+++ Protocolo.java
@@ -1,10 +1,25 @@
+import java.util.ArrayList; 
+
 public class Protocolo{
+	private ArrayList<String> contactos = new ArrayList<String>();
+	private ArrayList<String> emails = new ArrayList<String>();
 	private String user;
 	private String serverName;
 	private String password;
 
+	//SIMULANDO LA BASE DE DATOS
+	private void simularDB(){
+		contactos.add("pepito@115.01.3");
+		contactos.add("juan@101.80.9");
+		contactos.add("pablito@115.01.3");
+
+		emails.add("pablito@115.01.3 Hola Hola, como estas? Nos vemos pronto");
+		emails.add("juan@101.80.9 Salgamos Hola, te invito a mi fiesta el viernes");
+	}
+
 	//Funcion para cliente
 	public String LOGIN(String ingreso, String password){ //Recibe usuario@servidor
+		simularDB(); //Simulando base de datos
 		this.user = ingreso.split("@")[0];
 		this.serverName = ingreso.split("@")[1];
 		this.password = password;
@@ -37,8 +52,12 @@ public class Protocolo{
 	}
 	//Funcion para la DB en servidor
 	public String showCLIST(){
-		//Me retornas todos los contactos Server: info
-		return "andrea@server1 *";
+		//Me retorna todos los contactos Server: info
+		if (contactos.size() > 0) {
+			return contactos.remove(0);
+		} else {
+			return contactos.remove(0) + " *";
+		}
 	}
 	//Funcion para cliente
 	public String GETNEWMAILS(){
@@ -46,7 +65,11 @@ public class Protocolo{
 	}
 	//Funcion para la DB en servidor
 	public String showNewMails(){
-		//Me retornas todos los correos Server: info
-		return "usuario@server asunto cuerpo";
+		//Me retorna todos los correos Server: info
+		if (correos.size() > 0) {
+			return correos.remove(0);
+		} else {
+			return correos.remove(0) + " *";
+		}
 	}
 }
