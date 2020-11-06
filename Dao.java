4adf0920c7972a5feb9888655cdbf744cdb9b98a
Brandon Hernández <brandonhernandez_10@outlook.es>
2020-11-05 20:48:55 -0600

    DataBase

---
+++ Dao.java
@@ -0,0 +1,14 @@
+public class Dao {
+
+    private ServerDataBase dataBase;
+
+    public ServerDataBase getDataBase(){
+
+        if(dataBase == null){
+
+        }
+
+        return dataBase;
+    }
+
+}
