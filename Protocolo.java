0b8341d69fe5fe59f03cc8bed80002b76f245e99
andreamalin <andreaa.malin@gmail.com>
2020-11-07 17:16:30 -0600

    Ya casi termino las conexioens

--- Protocolo.java
+++ Protocolo.java
@@ -1,11 +1,17 @@
 import java.util.ArrayList; 
 
 public class Protocolo{
-	private String user;
-	private String serverName;
-	private String password;
+	private static String user;
+	private static String serverName;
+	private static String password;
 	private static boolean serverExists = false;
 
+	//Se colocan los datos del cliente desde el servidor
+	public void setClient(String user, String serverName, String password){
+		this.user = user;
+		this.serverName = serverName;
+		this.password = password;
+	}
 	//Funcion para cliente
 	public String checkServer(String ingreso, String password){ //Recibe usuario@servidor
 		this.user = ingreso.split("@")[0];
@@ -52,6 +58,40 @@ public class Protocolo{
 			return "OK LOGIN"; //Si no hay error, es porque ya ingreso el usuario
 		} else{
 			return "LOGIN ERROR 102"; //Esto lo retorna el server si no es correcta la contrasena
+		}
+	}
+	//Funcion para que el cliente pida el CLIST al server
+	public String CLIST(){
+		return "CLIST " + user;
+	}
+	//Funcion para el cliente pida los GETNEWMAILS al server
+	public String GETNEWMAILS(){
+		return "GETNEWMAILS " + user;
+	}
+	//Funcion para avisar al server que se mandara un correo
+	public String SENDMAIL(){
+		return "SEND MAIL";
+	}
+	//Funcion para avisar al server que se ingresara un nuevo contacto
+	public String NEWCONT(){
+		return "NEWCONT ";
+	}
+	//Funcion para avisar al server que se hara LOGOUT
+	public String LOGOUT(){
+		return "LOGOUT";
+	}
+	//Funcion para que el usuario le diga al cliente que hacer
+	public String buscarFuncion(String funExists){
+		if (funExists.equalsIgnoreCase("GETNEWMAILS")) {
+			return GETNEWMAILS(); //Se manda a llamar GETNEWMAILS para mandarle la senal al server
+		} else if (funExists.equalsIgnoreCase("SEND MAIL")) {
+			return SENDMAIL(); //Se manda a llamar SENDMAIL para mandarle la senal al server
+		} else if (funExists.equalsIgnoreCase("NEWCONT")) {
+			return NEWCONT(); //Se manda a llamar NEWCONT para mandarle la senal al server
+		} else if (funExists.equalsIgnoreCase("LOGOUT")) {
+			return LOGOUT(); //Se manda a llamar LOGOUT para mandarle la senal al server
+		} else {
+			return null; //Si no se encuentra la funcion, no se realiza nada
 		}
 	}
 }
